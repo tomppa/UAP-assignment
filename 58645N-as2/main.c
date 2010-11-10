9d0a462f4638dee09f67a063704df58946b977f2
Tuomas Hämäläinen <tuomas.83.hamalainen@gmail.com>
2010-11-10 11:46:21 +0200

    Changes from last week left uncommitted.

--- 58645N-as2/main.c
+++ 58645N-as2/main.c
@@ -5,6 +5,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <pthread.h>
 #include <signal.h>
 #include <string.h>
 
@@ -28,7 +29,7 @@ static void sig_hdlr(int signo)
 
     case SIGTSTP:
       printf("SIGTSTP received, see how the select loop works.\n");
-      write(fd_wr, "hello.\0", 6);
+      write(fd_wr, "hello\0", 6);
       break;
 
     default:
@@ -38,7 +39,26 @@ static void sig_hdlr(int signo)
   }
 }
 
-int handle_fds(fd_set tbr, struct arli list)
+void *process_data(void *threadarg)
+{
+  struct thread_data *my_data;
+  int taskid, test;
+  char *message;
+
+  my_data = (struct thread_data *) threadarg;
+  taskid = my_data->thread_id;
+  test = my_data->test;
+  message = my_data->message;
+
+  printf("Thread #%d getting command '%s', with test %d.\n", taskid, message,
+         test);
+
+  printf("Thread #%d exiting.\n", taskid);
+
+  pthread_exit(NULL);
+}
+
+int handle_fds(fd_set tbr, struct arli list, pthread_t threads[])
 {
   int *fds = list.values, i;
   char *tmp;
@@ -52,13 +72,28 @@ int handle_fds(fd_set tbr, struct arli list)
       errno = 0;
       int res = read(fds[i], tmp, _BUF_SIZE_);
 
-      if (res) {
+      if (res < 0) {
         perror("Reading the data from FD");
         return -1;
       }
 
-      else
-        printf("Read '%s' from FD.\n", tmp);
+      else {
+        int rc;
+        struct thread_data tda;
+
+        tda.thread_id = i;
+        tda.test = (i + 1) * 3;
+        tda.message = tmp;
+
+        printf("** DEBUG ** Message for thread #%d is '%s' and test is %d.\n",
+               i, tda.message, tda.test);
+        rc = pthread_create(&threads[i], NULL, process_data, (void *) &tda);
+        
+        if (rc < 0) {
+          perror("Creating thread");
+          return -1;
+        }
+      }
     }
   }
 
@@ -84,7 +119,7 @@ int otest(int *fd_tst)
     return -1;
   }
 
-  printf("Testfile opened. \n");
+  printf("Testfile opened.\n");
 
   return 0;
 }
@@ -116,10 +151,11 @@ void set_tv(struct timeval *tv)
 
 int main(void)
 {
-  fd_set tbr;
+  fd_set tbr; // FDs to be read after select stops blocking.
+  struct arli list; // Arraylist of all FDs given to select.
   struct timeval tv;
-  struct arli list;
   struct sigaction sig;
+  pthread_t threads[_NUM_THREADS_];
   int value, highfd, reset = 1;
 
   printf("Setting up signal handling.\n");
@@ -130,16 +166,22 @@ int main(void)
   sigaction(SIGINT, &sig, NULL);
   sigaction(SIGTSTP, &sig, NULL);
 
-  printf("Making the list.\n");
-
+  // Opening up a pipe.
   if (otest(&fd) < 0)
     exit(EXIT_FAILURE);
 
+  /* Open up a dummy write end to the fifo, so it can be guaranteed to stay
+   * open and in case all other write ends exit, select won't always return
+   * true (and thus never block) because of encountering EOF.
+   */
   if ((fd_wr = open(_TST_FILE_, O_WRONLY, S_IRWXU)) < 0)
-    perror("Open failure");
+    perror("Dummy write open failure");
+
+  printf("Making the list.\n");
 
   create(&list);
   add(&list, fd);
+  add(&list, fileno(stdin));
 
   tv.tv_sec = 0;
   tv.tv_usec = 0;
@@ -147,28 +189,25 @@ int main(void)
 
   int j = 0;
 
+  // Select loop.
   while (alive != 0 && j < 5)
   {
     j++;
 
     init_tbr(&tbr, list);
+
+    // If loop finished before timeout ended, don't reset timer.
     if (reset == 1)
       set_tv(&tv);
     else
       reset = 1;
 
-    #ifdef _DEBUG_
-    printf("*** DEBUG *** Time for sleeping: %d.%ds.\n", (int) tv.tv_sec,
-           (int) tv.tv_usec);
-    #endif //_DEBUG_
-
     value = select(highfd + 1, &tbr, NULL, NULL, &tv);
 
     if (value == -1) {
-      if (errno == EINTR) {
-        // Interrupted by an incoming signal.
+      // Interrupted by an incoming signal.
+      if (errno == EINTR)
         reset = 0;
-      }
       
       else {
         perror("Problem with select()");
@@ -182,7 +221,7 @@ int main(void)
     else {
       printf("Things to do, yay!\n");
       reset = 0;
-      handle_fds(tbr, list);
+      handle_fds(tbr, list, threads);
     }
   }
 
@@ -199,8 +238,8 @@ int main(void)
     printf("Fifo removed.\n");
 
   close(fd_wr);
-  printf("Terminating.\n");
+  printf("Main thread terminating.\n");
 
-  return 0;
+  pthread_exit(NULL);
 }
 
--- 58645N-as2/main.h
+++ 58645N-as2/main.h
@@ -7,9 +7,16 @@
 #define _BUF_SIZE_ 81
 #define _SEC_ 3
 #define _USEC_ 151592
+#define _NUM_THREADS_ 5
 #endif
 
-extern int handle_fds(fd_set, struct arli);
+struct thread_data {
+  int thread_id;
+  int test;
+  char *message;
+};
+
+extern int handle_fds(fd_set, struct arli, pthread_t threads[]);
 extern void init_tbr(fd_set*, struct arli);
 extern void set_tv(struct timeval*);
 
